bffa180c8bc888ad4b9e86a3110f5b136f67c232
etuong <etuong@gmail.com>
2022-06-23 20:05:36 -0700

    Bulma layout on home page

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="section">
+    <div
+      class="pt-5"
+      style="border-top: 1px solid #dee2e6;"
+    />
+    <div class="container">
+      <div class="is-flex-tablet is-justify-content-end is-align-items-center">
+        <p>
+          Created by
+          <a href="https://github.com/etuong/cards-for-humanity">Ethan Uong</a> 👨‍💻
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FooterComponent",
+});
+</script>
+
+<style scoped>
+
+</style>
